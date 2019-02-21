cbc274b0d1d386543a49cb62f89730d996d930db
bit <bit@175.209.90.168>
2019-02-21 10:45:20 +0900

    1

--- src/main/java/problem01/Main.java
+++ src/main/java/problem01/Main.java
@@ -1,5 +1,7 @@
 package problem01;
 
+import java.util.Arrays;
+
 public class Main {
 	public static void main(String[] args) {
 		int[] arr = { -10, -3, 5, 6, -20 };
@@ -7,10 +9,43 @@ public class Main {
 	}
 
 	public static void findMaxProduct(int[] arr) {
-		
+
 		//
 		// 코드를 완성 하십시오.
 		//
+
+		int[] mul = new int[10];
+
+		int num = 0;
+		// 모든 곱의 경우의 수
+		for (int i = 0; i < 5; i++) {
+			for (int j = i + 1; j < 5; j++) {
+				mul[num++] = arr[i] * arr[j];
+				// System.out.println(mul[i+j-1]);
+			}
+			// System.out.println();
+		}
+
 		
-	}
+		//곱의 최대값
+		int max = mul[0];
+		int pos = 0;
+	    for (int i = 0; i < mul.length; i++) {
+	    	if (mul[i] > max) {
+	            max = mul[i];
+	            pos = i;
+	        }
+	    }
+	    //System.out.println(max);
+	    
+	    
+	    //결과
+	    if(pos<=3) {
+	    	System.out.println("[" + max/arr[pos+1] + "," + arr[pos+1] +"]");
+	    } else if(pos<=6) {
+	    	System.out.println("[" + max/arr[pos+1] + "," + arr[pos-2] +"]");   
+	    } else 
+	    	System.out.println("[" + max/arr[pos+1] + "," + arr[pos-4] +"]");
+	    }
+
 }
--- src/main/java/problem02/Main.java
+++ src/main/java/problem02/Main.java
@@ -16,7 +16,9 @@ public class Main {
 			// num1, num2, arith 세 개의 변수와 getInstance 메소드를 사용하여
 			// 문제의 결과화면에 맞게 출력하는 계산기 나머지 코드를 아래에 완성 하십시오.
 			// 주어진 코드는 수정하지 않습니다.
-
+			
+			
+			System.out.println(getInstance(arith).calculate(num1, num2));
 		
 		
 		}
@@ -24,8 +26,46 @@ public class Main {
 	
 	public static Arithmetic getInstance(String arith) {
 		
-		// 메소드 구현을 완성 하십시오.
+		switch (arith) {
+		case "+" :  Arithmetic add = new Add();
+			return add;
+			
+		case "-" :  Arithmetic sub = new Sub();
+			return sub;
+			
+		case "*" :  Arithmetic mul = new Mul();
+			return mul;
+			
+		case "/" :  Arithmetic div = new Div();
+			return div;
 		
+		}	
 		return null;
+	}
+	
+	// 사친연산 클래스
+	public static class Add implements Arithmetic {
+		@Override
+		public int calculate(int a, int b) {
+			return a+b;
+		}		
+	}	
+	public static class Sub implements Arithmetic {
+		@Override
+		public int calculate(int a, int b) {
+			return a-b;
+		}		
+	}	
+	public static class Mul implements Arithmetic {
+		@Override
+		public int calculate(int a, int b) {
+			return a*b;
+		}		
+	}
+	public static class Div implements Arithmetic {
+		@Override
+		public int calculate(int a, int b) {
+			return a/b;
+		}		
 	}
 }
